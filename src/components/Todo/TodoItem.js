f228737991decb6a3107320dc9ae047f65af89a3
mohcine <mohcine.nahiddd@gmail.com>
2023-04-05 16:05:34 +0200

    mohcine

---
+++ src/components/Todo/TodoItem.js
@@ -0,0 +1,39 @@
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import{
+    faCheck,
+    faPenToSquare,
+    faTrashAlt,
+    faXmark,
+}from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const TodoItem = (props) => {
+  return (
+    <ul className="list-group todos mx-auto text-light">
+      <li
+        className={`list-group-item d-flex justify-content-between align-items-center ${props.item.complete === true ? 'item-complete':""}`}
+      >
+        <span>{props.item.todo}</span>
+        <div>
+          <FontAwesomeIcon
+            style={{
+              marginRight: "0.3em",
+            }}
+            icon={props.item.complete === true ? faXmark : faCheck}
+            className="pointer"
+            onClick={()=>props.completeTodoItem(props.item.id)}
+          />
+          <FontAwesomeIcon
+            style={{
+              marginRight: "0.3em",
+            }}
+            icon={faPenToSquare}
+            className="pointer"
+          />
+          <FontAwesomeIcon icon={faTrashAlt} className="pointer" />
+        </div>
+      </li>
+    </ul>
+  );
+};
+export default TodoItem;
